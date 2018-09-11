801efca7cd0ae7f79d1a79c7e730edc573413b0c
Marte Roel <marteroel@users.noreply.github.com>
2018-09-11 12:49:19 +0200

    Imported IK and vive avatar

---
+++ AR-Vive/Assets/Vive body tracking/Scripts/AvatarScaler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarScaler : MonoBehaviour {
+
+	public GameObject avatar;
+	public Transform originalFootPos, originalHeadPos, userFootPos, userHeadPos;
+
+	private float originalSize, userSize;
+	private bool isResizing = false;
+	private Transform initialAvatarScale;
+
+	// Use this for initialization
+	void Start () {
+		
+		originalSize = Vector3.Distance (originalFootPos.position, originalHeadPos.position);
+
+		//initialAvatarScale.localScale = new Vector3 (1f, 1f, 1f);
+
+		//initialAvatarScale.localScale = avatar.transform.localScale;
+		//Debug.Log ("The initial avatar scale is " + initialAvatarScale.localScale);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (isResizing) {
+			Resize ();
+		}
+
+		if (Input.GetKeyDown ("space"))
+			isResizing = !isResizing;
+
+	}
+
+	public void Resize() {
+		userSize = Vector3.Distance (userFootPos.position, userHeadPos.position);
+		float ratio = userSize / originalSize;
+		//Debug.Log (ratio);
+		//avatar.transform.localScale = initialAvatarScale.localScale * ratio;
+		avatar.transform.localScale = new Vector3 (1f, 1f, 1f) * ratio;
+
+	}
+
+
+
+}
---
+++ AR-Vive/Assets/Vive body tracking/Scripts/ControllerButtonManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerButtonManager : MonoBehaviour {
+
+	private SteamVR_TrackedController device;
+	public static bool triggerIsPressed;
+	public static bool padIsPressed;
+
+	// Use this for initialization
+	void Start () {
+		device = GetComponent<SteamVR_TrackedController>();
+
+		device.TriggerClicked += Trigger;
+		device.TriggerUnclicked += Trigger;
+
+		device.PadClicked += Pad;
+		device.PadUnclicked += Pad;
+	}
+
+	void Trigger(object sender, ClickedEventArgs e) {
+		
+		triggerIsPressed = !triggerIsPressed;
+		Debug.Log("Trigger has been pressed " +  triggerIsPressed);
+	}
+
+	void Pad(object sender, ClickedEventArgs e) {
+
+		padIsPressed = !padIsPressed;
+		Debug.Log("Pad has been pressed " +  padIsPressed);
+	}
+		
+}
