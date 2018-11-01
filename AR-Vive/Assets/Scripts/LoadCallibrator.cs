e6454ce216426e720912540f2259d1cdf07f5be3
Body-self-and-plasticity-Lab <33545780+BSP-Lab@users.noreply.github.com>
2018-11-01 13:15:44 +0100

    Added scene for calibrating trackers
    Controllers are necessary so far

---
+++ AR-Vive/Assets/Scripts/LoadCallibrator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadCallibrator : MonoBehaviour {
+
+	void Update() {
+		
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			SceneManager.LoadScene ("Intro");
+		}
+	}
+
+	public void LoadCallibratorScene(){
+		SceneManager.LoadScene ("VR_Setup");
+	}
+}
